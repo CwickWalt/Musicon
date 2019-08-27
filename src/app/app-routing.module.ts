e331102be9d6e602615707172212b1211b384000
CwickWalt <chadwalt21@gmail.com>
2019-08-26 20:16:52 -0400

    create landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'playlists',
+    component: PlaylistsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
